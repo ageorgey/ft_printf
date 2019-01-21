65a739be25d69611a172c69535026e037b00a543
Adam Georgey <adam.georgey@gmail.com>
2019-01-21 23:02:35 +0100

    loading ...

--- sources/gestion_convert.c
+++ sources/gestion_convert.c
@@ -8,5 +8,58 @@ s_format			*gestion_type(s_format *sf, va_list ap)
 	else if (sf->type[0] == 'd' || sf->type[0] == 'i' || sf->type[0] == 'o'
 		|| sf->type[0] == 'u' || sf->type[0] == 'x' || sf->type[0] == 'b')
 		convert_diouxXb(sf, ap);
-	
+	else if (sf->type[0] == 'f')
+		convert_f(sf, ap);
+	return (sf);
+}
+
+s_format			*gestion_size(s_format *sf, va_list ap)
+{
+	if (sf->type[0] == 'd' || sf->type[0] == 'i')
+		size_for_di(sf, ap);
+	else if (sf->type[0] == 'o')
+		size_for_o(sf, ap);
+	else if (sf->type[0] == 'u')
+		size_for_u(sf, ap);
+	else if (sf->type[0] == 'x' || sf->type[0] == 'X')
+		size_for_xX(sf, ap);
+	else if (sf->type[0] == 'b')
+		size_for_b(sf, ap);
+	else if (sf->type[0] == 'f')
+		size_for_f(sf, ap);
+	return (sf);
+}
+
+s_format			*gestion_precision(s_format *sf)
+{
+	if (sf->type[0] == 's')
+		precision_for_s(sf);
+	else if (sf->type[0] == 'f')
+		precision_for_f(sf);
+	else
+		precision_for_diouxXb(sf);
+	return (sf);
+}
+
+s_format			*gestion_flags_width(s_format *sf)
+{
+	if (sf->type[0] == 'o' || sf->type[0] == 'x' || sf->type[0] == 'X')
+		flag_sharp_for_oxX(sf);
+	else if (sf->type[0] == 'f')
+		flag_sharp_for_f(sf);
+	else if (sf->type[0] == 'd' || sf->type[0] == 'i' || sf->type[0] == 'f')
+		flag_positive(sf);
+	width_for_all(sf);
+	return (sf);
+}
+
+s_format			*gestion_all(s_format *sf, va_list ap)
+{
+	if (!sf->size)
+		gestion_type(sf, ap);
+	else if (sf->size)
+		gestion_size(sf, ap);
+	gestion_precision(sf);
+	gestion_flags_width(sf);
+	return (sf);
 }
